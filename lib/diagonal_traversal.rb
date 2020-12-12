44386ce0854c22ed2e004ba5d21cfae2e9c67790
CheezItMan <chris@adadevelopersacademy.org>
2020-12-12 12:00:54 -0800

    initial diagonal traversal

---
+++ lib/diagonal_traversal.rb
@@ -0,0 +1,14 @@
+
+# Input:  A matrix of M x N elements (M rows, N columns), 
+# Return value:  All elements of the matrix in diagonal order.
+#  Example input:
+# [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+# ]
+# Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
+
+def diagonal_traversal(matrix)
+  raise NotImplementedError, "This method has not yet been implemented"
+end
